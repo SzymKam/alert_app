ab2f33c29e66349dbef5e7e886bcf8ccc00a0c68
SzymKam <szymon15kaminski@gmail.com>
2024-02-26 15:25:05 +0100

    feat: add mutation - create and update model

--- src/app/schema.py
+++ src/app/schema.py
@@ -21,63 +21,80 @@ class Query(graphene.ObjectType):
         return RescueContact.objects.get(id=id)
 
 
-#
-#
-# class CreateRescueContact(graphene.Mutation):
-#     rescue_contact = graphene.Field(RescueContactType)
-#
-#     class Arguments:
-#         first_name = graphene.String(required=True)
-#         last_name = graphene.String(required=True)
-#         medical_qualifications = graphene.String()
-#         is_driver = graphene.Boolean()
-#         has_vehicle_driving_privileges = graphene.Boolean()
-#         is_navigator = graphene.Boolean()
-#         is_planner = graphene.Boolean()
-#
-#     def mutate(self, info, first_name, last_name, medical_qualifications, is_driver,
-#                has_vehicle_driving_privileges, is_navigator, is_planner):
-#         new_rescue_contact = RescueContact(
-#             first_name=first_name,
-#             last_name=last_name,
-#             medical_qualifications=medical_qualifications,
-#             is_driver=is_driver,
-#             has_vehicle_driving_privileges=has_vehicle_driving_privileges,
-#             is_navigator=is_navigator,
-#             is_planner=is_planner
-#         )
-#         new_rescue_contact.save()
-#         return CreateRescueContact(rescue_contact=new_rescue_contact)
-#
-#
-# class UpdateRescueContact(graphene.Mutation):
-#     ok = graphene.Boolean()
-#     rescue_contact = graphene.Field(RescueContactType)
-#
-#     class Arguments:
-#         id = graphene.ID()
-#         first_name = graphene.String()
-#         last_name = graphene.String()
-#         medical_qualifications = graphene.String()
-#         is_driver = graphene.Boolean()
-#         has_vehicle_driving_privileges = graphene.Boolean()
-#         is_navigator = graphene.Boolean()
-#         is_planner = graphene.Boolean()
-#
-#     def mutate(self, info, first_name, last_name, medical_qualifications, is_driver,
-#                has_vehicle_driving_privileges, is_navigator, is_planner, id):
-#         update_rescue_contact = RescueContact.objects.get(id=id)
-#         update_rescue_contact.first_name = first_name
-#         update_rescue_contact.last_name = last_name
-#         update_rescue_contact.medical_qualifications = medical_qualifications
-#         update_rescue_contact.is_driver = is_driver
-#         update_rescue_contact.has_vehicle_driving_privileges = has_vehicle_driving_privileges
-#         update_rescue_contact.is_navigator = is_navigator
-#         update_rescue_contact.is_planner = is_planner
-#         update_rescue_contact.save()
-#         return UpdateRescueContact(ok=True, rescue_contact=update_rescue_contact)
-#
-#
+class CreateRescueContact(graphene.Mutation):
+    rescue_contact = graphene.Field(RescueContactType)
+
+    class Arguments:
+        first_name = graphene.String(required=True)
+        last_name = graphene.String(required=True)
+        medical_qualifications = graphene.String()
+        is_driver = graphene.Boolean()
+        has_vehicle_driving_privileges = graphene.Boolean()
+        is_navigator = graphene.Boolean()
+        is_planner = graphene.Boolean()
+
+    def mutate(
+        self,
+        info,
+        first_name,
+        last_name,
+        medical_qualifications,
+        is_driver,
+        has_vehicle_driving_privileges,
+        is_navigator,
+        is_planner,
+    ):
+        new_rescue_contact = RescueContact(
+            first_name=first_name,
+            last_name=last_name,
+            medical_qualifications=medical_qualifications,
+            is_driver=is_driver,
+            has_vehicle_driving_privileges=has_vehicle_driving_privileges,
+            is_navigator=is_navigator,
+            is_planner=is_planner,
+        )
+        new_rescue_contact.save()
+        return CreateRescueContact(rescue_contact=new_rescue_contact)
+
+
+class UpdateRescueContact(graphene.Mutation):
+    ok = graphene.Boolean()
+    rescue_contact = graphene.Field(RescueContactType)
+
+    class Arguments:
+        id = graphene.ID()
+        first_name = graphene.String()
+        last_name = graphene.String()
+        medical_qualifications = graphene.String()
+        is_driver = graphene.Boolean()
+        has_vehicle_driving_privileges = graphene.Boolean()
+        is_navigator = graphene.Boolean()
+        is_planner = graphene.Boolean()
+
+    def mutate(
+        self,
+        info,
+        first_name,
+        last_name,
+        medical_qualifications,
+        is_driver,
+        has_vehicle_driving_privileges,
+        is_navigator,
+        is_planner,
+        id,
+    ):
+        update_rescue_contact = RescueContact.objects.get(id=id)
+        update_rescue_contact.first_name = first_name
+        update_rescue_contact.last_name = last_name
+        update_rescue_contact.medical_qualifications = medical_qualifications
+        update_rescue_contact.is_driver = is_driver
+        update_rescue_contact.has_vehicle_driving_privileges = has_vehicle_driving_privileges
+        update_rescue_contact.is_navigator = is_navigator
+        update_rescue_contact.is_planner = is_planner
+        update_rescue_contact.save()
+        return UpdateRescueContact(ok=True, rescue_contact=update_rescue_contact)
+
+
 # class DeleteRescueContact(graphene.Mutation):
 #     ok = graphene.Boolean()
 #
@@ -91,7 +108,9 @@ class Query(graphene.ObjectType):
 #
 #
 #
-# class Mutation(graphene.ObjectType):
-#     create_rescue_contact = CreateRescueContact.Field()
-#     update_rescue_contact = UpdateRescueContact.Field()
+class Mutation(graphene.ObjectType):
+    create_rescue_contact = CreateRescueContact.Field()
+    update_rescue_contact = UpdateRescueContact.Field()
+
+
 #     delete_rescue_contact = DeleteRescueContact.Field()
