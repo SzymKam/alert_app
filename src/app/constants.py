91a48b05d02d97d22da69a95f1b9cbe252304e24
SzymKam <szymon15kaminski@gmail.com>
2024-02-26 15:17:23 +0100

    feat: add medical qualifications list

---
+++ src/app/constants.py
@@ -0,0 +1,10 @@
+QUALIFICATIONS = sorted(
+    [
+        ("Qualified first aid", "Qualified first aid"),
+        ("Paramedic", "Paramedic"),
+        ("Nurse", "Nurse"),
+        ("Doctor", "Doctor"),
+        ("First aid", "First aid"),
+        ("Midwife", "Midwife"),
+    ]
+)
