b757e8793d37920c934e66e8bea9a1e3138cef13
SzymKam <szymon15kaminski@gmail.com>
2024-02-27 06:46:19 +0100

    feat: add new schema for sending alarm message. Use Mutation class

---
+++ src/app/schema/send_alarm_schema.py
@@ -0,0 +1,21 @@
+import graphene
+
+from ..views import SendingAlarmMessages
+
+
+class SendAlarm(graphene.Mutation):
+    confirmation = graphene.String()
+
+    class Arguments:
+        ids = graphene.List(graphene.Int)
+        message = graphene.String()
+
+    def mutate(self, info, ids, message):
+        sending_alarm = SendingAlarmMessages(ids=ids, message=message)
+        alarmed_contacts = sending_alarm.main()
+
+        return SendAlarm(confirmation=f"Message: {message} sended to {alarmed_contacts}")
+
+
+class Mutation(graphene.ObjectType):
+    send_alarm = SendAlarm.Field()
