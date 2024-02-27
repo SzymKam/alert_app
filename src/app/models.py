93e38df2a2ed532610622682f3f30c0ac8f401de
SzymKam <szymon15kaminski@gmail.com>
2024-02-27 06:47:00 +0100

    feat: add new fields in model: email and phone

--- src/app/models.py
+++ src/app/models.py
@@ -13,6 +13,8 @@ class RescueContact(models.Model):
     has_vehicle_driving_privileges = models.BooleanField(default=False)
     is_navigator = models.BooleanField(default=False)
     is_planner = models.BooleanField(default=False)
+    email = models.EmailField(max_length=254, unique=True, null=True, blank=True)
+    phone = models.CharField(max_length=12, unique=True, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.medical_qualifications}"
