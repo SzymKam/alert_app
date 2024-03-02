a53bcf54cbbea24ef91d22c467a95dec856bd69b
SzymKam <szymon15kaminski@gmail.com>
2024-03-02 22:14:38 +0100

    fix: update imported schema

--- src/core/schema.py
+++ src/core/schema.py
@@ -1,7 +1,7 @@
 import graphene
 
-from app.schema.rescue_contact_schema import Mutation as RescueContactMutation
-from app.schema.rescue_contact_schema import Query as RescueContactQuery
+from app.schema.mongo_db_rescue_contact_schema import Mutation as RescueContactMutation
+from app.schema.mongo_db_rescue_contact_schema import Query as RescueContactQuery
 from app.schema.send_alarm_schema import Mutation as SendAlarmMutation
 
 
@@ -9,7 +9,7 @@ class Query(RescueContactQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(RescueContactMutation, SendAlarmMutation, graphene.ObjectType):
+class Mutation(SendAlarmMutation, RescueContactMutation, graphene.ObjectType):
     pass
 
 
