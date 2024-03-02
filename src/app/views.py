248bc111865001405c312f9d58ade644bb2f517a
SzymKam <szymon15kaminski@gmail.com>
2024-03-02 22:16:19 +0100

    fix/feat: update view -> catch errors

--- src/app/views.py
+++ src/app/views.py
@@ -12,49 +12,38 @@ class SendingAlarmMessages:
     def __init__(self, ids, message):
         self.__ids = ids
         self.__message = message
+        # self.__e
 
     def main(self):
         self.__get_contacts()
-        self.__get_email_phone_lists()
-        self.__send_email()
-        self.__send_sms()
 
         return self.contacts
 
     def __get_contacts(self):
         self.contacts = RescueContact.objects.filter(id__in=self.__ids)
 
-    def __get_email_phone_lists(self):
-        self.__email_list = [contact.email for contact in self.contacts]
-        self.__phone_list = [contact.phone for contact in self.contacts]
+    def __send_email(self):
+        try:
+            message = EmailMessage(
+                subject="Rescue Alert",
+                body=self.__message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[contact.email for contact in self.contacts],
+            )
+            message.send()
+            return True
 
-    #
-    # def __send_email(self):
-    #     try:
-    #         message = EmailMessage(
-    #             subject='Rescue Alert',
-    #             body=self.__message,
-    #             from_email=settings.DEFAULT_FROM_EMAIL,
-    #             to=self.__email_list,
-    #         )
-    #         message.send()
-    #         return True
-    #
-    #     except SMTPException as e:
-    #         print(f"SMTPException: {e}")
-    #         return False
-    #
-    #
-    # def __send_sms(self):
-    #     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    #     for phone in self.__phone_list:
-    #         try:
-    #             client.messages.create(
-    #                 to=phone,
-    #                 from_=settings.TWILIO_PHONE_NUMBER,
-    #                 body=self.__message
-    #             )
-    #
-    #         except TwilioException as e:
-    #             print(f"TwilioException: {e}")
-    #             continue
+        except SMTPException as e:
+            print(f"SMTPException: {e}")
+            return e
+
+    def __send_sms(self):
+        # confirmation = dict()
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        for contact in self.contacts:
+            try:
+                client.messages.create(to=contact.phone, from_=settings.TWILIO_PHONE_NUMBER, body=self.__message)
+
+            except TwilioException as e:
+                print(f"TwilioException: {e}")
+                continue
