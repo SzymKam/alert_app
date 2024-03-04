7a2012954388134531c6198d766e410a6c6d6c3d
SzymKam <szymon15kaminski@gmail.com>
2024-03-04 11:25:31 +0100

    fix: change name of some fields in model

--- src/app/models.py
+++ src/app/models.py
@@ -9,10 +9,10 @@ class RescueContact(Document):
     medical_qualifications = StringField(
         max_length=50, choices=QUALIFICATIONS, default="Qualified first aid", help_text="Qualifications"
     )
-    is_driver = BooleanField(default=False)
-    has_vehicle_driving_privileges = BooleanField(default=False)
-    is_navigator = BooleanField(default=False)
-    is_planner = BooleanField(default=False)
+    driver = BooleanField(default=False)
+    driving_privileges = BooleanField(default=False)
+    navigator = BooleanField(default=False)
+    planner = BooleanField(default=False)
     email = EmailField(max_length=254, unique=True)
     phone = StringField(max_length=12, unique=True)
 
