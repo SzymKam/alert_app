1dcab2e516d4f5a10fcf319a7e3553bbe57852c8
SzymKam <szymon15kaminski@gmail.com>
2024-03-05 00:08:49 +0100

    fix: update variables

--- src/core/alert_app_env.py
+++ src/core/alert_app_env.py
@@ -2,7 +2,7 @@ from django.core.management.utils import get_random_secret_key
 from environ import Env
 
 env = Env(
-    DEBUG=(bool, False),
+    DEBUG=(bool, True),
     SECRET_KEY=(str, get_random_secret_key()),
     EMAIL_HOST_PASSWORD=(str, "your_email_host_user_password"),
     EMAIL_HOST_USER=(str, "your_email_host_user"),
@@ -10,7 +10,7 @@ env = Env(
     TWILIO_ACCOUNT_SID=(str, "your_twilio_account_sid"),
     TWILIO_AUTH_TOKEN=(str, "your_twilio_auth_token"),
     TWILIO_PHONE_NUMBER=(str, "your_twilio_phone_number"),
-    MONGODB_NAME=(str, "your_mongodb_database_name"),
-    MONGODB_HOST=(str, "your_mongodb_database_host"),
-    MONGODB_PORT=(int, "default=27017"),
+    MONGODB_NAME=(str, "alert_app_db_mongo"),
+    MONGODB_HOST=(str, "db"),
+    MONGODB_PORT=(int, 27017),
 )
